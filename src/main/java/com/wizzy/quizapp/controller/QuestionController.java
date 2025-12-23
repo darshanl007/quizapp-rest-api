19ccde750a16d9e57f08051dfe61e1fc7a9d3d8d
Darshan L <darshanl1254@gmail.com>
2025-12-23 14:34:31 +0530

    Add question

--- src/main/java/com/wizzy/quizapp/controller/QuestionController.java
+++ src/main/java/com/wizzy/quizapp/controller/QuestionController.java
@@ -1,10 +1,28 @@
 package com.wizzy.quizapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.wizzy.quizapp.entity.Question;
+import com.wizzy.quizapp.service.QuestionService;
 
 @RestController
 @RequestMapping("/api/questions")
 public class QuestionController {
-	
+
+	@Autowired
+	QuestionService questionService;
+
+	// Add Question
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Question addQuestion(@RequestBody Question question) {
+		return questionService.addQuestion(question);
+	}
+
 }
--- src/main/java/com/wizzy/quizapp/service/QuestionService.java
+++ src/main/java/com/wizzy/quizapp/service/QuestionService.java
@@ -1,8 +1,19 @@
 package com.wizzy.quizapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.wizzy.quizapp.entity.Question;
+import com.wizzy.quizapp.repository.QuestionRepository;
 
 @Service
 public class QuestionService {
+
+	@Autowired
+	QuestionRepository questionRepository;
+
+	public Question addQuestion(Question question) {
+		return questionRepository.save(question);
+	}
 
 }
