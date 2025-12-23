c2493604d98592c12a347509b9052f2fe077e2b9
Darshan L <darshanl1254@gmail.com>
2025-12-23 14:40:23 +0530

    Get all questions

--- src/main/java/com/wizzy/quizapp/controller/QuestionController.java
+++ src/main/java/com/wizzy/quizapp/controller/QuestionController.java
@@ -1,7 +1,10 @@
 package com.wizzy.quizapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,4 +28,10 @@ public class QuestionController {
 		return questionService.addQuestion(question);
 	}
 
+	// Get All Questions
+	@GetMapping
+	@ResponseStatus(HttpStatus.OK)
+	public List<Question> getQuestions() {
+		return questionService.getAllQuestions();
+	}
 }
